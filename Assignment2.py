f08b89d4321bf1dabda124bd2585f5539778bda4
tenyv <tenyvarghese25@gmail.com>
2026-02-25 21:35:26 +0530

    Assignment 2 Submission

---
+++ Assignment2.py
@@ -0,0 +1,8 @@
+#Task 2 Write a Python program that: Takes a user's first name and last name as input.
+#Concatenates the first name and last name into a full name.
+#Prints a personalized greeting message using the full name
+
+first_name=input("Enter your first name: ")
+last_name=input("Enter your last name: ")
+
+print("Hello,",first_name,last_name+"!","Welcome to Python Programing.")
