840c9f67a4ae7d09f2425a3897e00b578088a917
tenyv <tenyvarghese25@gmail.com>
2026-02-25 10:51:36 +0530

    Assignment 1 Submission

---
+++ Assignment1.py
@@ -0,0 +1,14 @@
+##Task 1: Perform Basic Mathematical Operations Using Input and print functions
+
+num1= input("Enter the first number: ")
+num2= input("Enter the second number: ")
+
+add= int(num1) + int(num2)
+diff= int(num1) - int(num2)
+multiply= int(num1) * int(num2)
+division= int(num1) / int(num2)
+
+print("Addition is: ", add)
+print("Difference is: ", diff)
+print("Multiplication is: ", multiply)
+print("Division is: ", division)
