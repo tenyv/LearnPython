85784b63a272949677f459b3d06420b95bb1d1c8
tenyv <tenyvarghese25@gmail.com>
2026-02-26 09:35:59 +0530

    Made changes based on feedback recieved

--- Assignment1.py
+++ Assignment1.py
@@ -1,14 +1,17 @@
 ##Task 1: Perform Basic Mathematical Operations Using Input and print functions
 
-num1= input("Enter the first number: ")
-num2= input("Enter the second number: ")
+# Get Input from User For 2 numbers and convert the input to integer using typecast
+num1= int(input("Enter the first number: "))
+num2= int(input("Enter the second number: "))
 
-add= int(num1) + int(num2)
-diff= int(num1) - int(num2)
-multiply= int(num1) * int(num2)
-division= int(num1) / int(num2)
+#Person mathematical operation on the numbers recived
+addition= num1 + num2
+difference= num1 - num2
+multiply= num1 * num2
+division= num1 / num2
 
-print("Addition is: ", add)
-print("Difference is: ", diff)
+#Print the result of each operation back to use on console
+print("Addition is: ", addition)
+print("Difference is: ", difference)
 print("Multiplication is: ", multiply)
 print("Division is: ", division)
--- Assignment2.py
+++ Assignment2.py
@@ -2,7 +2,9 @@
 #Concatenates the first name and last name into a full name.
 #Prints a personalized greeting message using the full name
 
+# Get Input from User
 first_name=input("Enter your first name: ")
 last_name=input("Enter your last name: ")
 
+# Print User's Name with greeting
 print("Hello,",first_name,last_name+"!","Welcome to Python Programing.")
